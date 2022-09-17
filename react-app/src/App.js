9b480abc074521f7a3f710c7b3de2a17843c0591
steven bai <stevenbai0724@gmail.com>
2022-09-17 18:51:20 -0400

    file upload and 4 columns

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,33 +1,106 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 
 function App() {
-  const [val, setVal] = useState();
-  const [img, setImg] = useState();
 
-  const handleOnChange = (e) => {
-    setVal(e.target.value);
+  const [images, setImages] = useState([])
+  const [imageUrls, setImageUrls] = useState([])
+  function onImageChange(e) {
+    setImages([...images, ...e.target.files])
   }
-  const search = evt => {
-    if(evt.key === "Enter"){
-        setImg(val) //state changed to re-render the api call
-    }
-}
+
+  useEffect(() => {
+    if (images.length < 1)return; //don't run when the array is empty
+    const newImageUrls = [];
+    images.forEach(image => {
+      newImageUrls.push(URL.createObjectURL(image))
+      console.log(URL.createObjectURL(image))
+    });
+    setImageUrls(newImageUrls)
+  }, [images])
+
   return (
     <div className="App">
-        <input 
-          className='input'
-          type = "text"
-          placeholder = "enter link"
-          onChange = {handleOnChange}
-          value = {val}
-          onKeyPress = {search}
-          
-        /> 
+      <div className = "Header">
 
-        <img src = {img}></img>
+      </div>
+      <div className = "Body">
+        <div className = "Left">
+
+          <div className='Title'>
+            Hack The North 2022: Branding
+          </div>
+
+
+          
+
+
+
+          <div className = "Elements">
+
+            
+
+
+                <div className = "Col">
+                  <div style = {{width: "90%", borderWidth: "2px", borderStyle: "solid", height: "50px"}}>
+                    <input type="file" multiple accept="image/*" onChange={onImageChange} />
+                  </div>
+
+                  {imageUrls.map((imageSrc, index) => 
+                    {return(
+                      
+                      (index % 4) === 0 && <img style = {{marginBottom: "15px", width: "90%"}} key = {index} src = {imageSrc}></img>
+                    
+                    )
+                    }
+                  )}
+
+                </div>
+                <div className = "Col">
+                  {imageUrls.map((imageSrc, index) => 
+                      {return(
+                        
+                        (index % 4) === 1 && <img style = {{marginBottom: "15px", width: "90%"}} key = {index} src = {imageSrc}></img>
+                      
+                      )
+                      }
+                  )}
+                </div>
+                <div className = "Col">
+                  {imageUrls.map((imageSrc, index) => 
+                      {return(
+                        
+                        (index % 4) === 2 && <img style = {{marginBottom: "15px", width: "90%"}} key = {index} src = {imageSrc}></img>
+                      
+                      )
+                      }
+                  )}
+                </div>
+                <div className = "Col">
+                  {imageUrls.map((imageSrc, index) => 
+                      {return(
+                        
+                        (index % 4) === 3 && <img style = {{marginBottom: "15px", width: "90%"}} key = {index} src = {imageSrc}></img>
+                      
+                      )
+                      }
+                  )}  
+                </div>
+
+
+              
+              
+          </div>
+
+
+        </div>
+
+        <div className = "Right">
+          side bar
+        </div>
+      </div>
       
     </div>
   );
